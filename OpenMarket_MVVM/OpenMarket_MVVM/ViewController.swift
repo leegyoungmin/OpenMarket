39f40eb168771553414d0642da267d29cd2b4908
미니 <cow970814@naver.com>
2023-03-21 17:15:09 +0900

    feat : Start Project

---
+++ OpenMarket_MVVM/OpenMarket_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenMarket_MVVM
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
