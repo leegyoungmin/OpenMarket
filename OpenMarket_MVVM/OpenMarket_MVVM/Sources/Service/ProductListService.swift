76c3b2aeb77579ae54ef7d5b5bc3b7fdf49c8437
미니 <cow970814@naver.com>
2023-03-23 15:48:00 +0900

    feat : Product List View Model Pagenation Properties 생성 및 적용

--- OpenMarket_MVVM/OpenMarket_MVVM/Sources/Service/ProductListService.swift
+++ OpenMarket_MVVM/OpenMarket_MVVM/Sources/Service/ProductListService.swift
@@ -9,22 +9,19 @@ import Combine
 
 protocol ProductListServicing {
     var productsListRepository: ProductListLoadable { get }
-    func loadProducts() -> AnyPublisher<[Product], Error>
+    func loadProducts(pageNumber: Int, count: Int) -> AnyPublisher<[Product], Error>
 }
 
 final class ProductListService: ProductListServicing {
-    private var pageNumber: Int = 1
-    private var elementCount: Int = 10
-    
     private(set) var productsListRepository: ProductListLoadable
     
     init(productsListRepository: ProductListLoadable) {
         self.productsListRepository = productsListRepository
     }
     
-    func loadProducts() -> AnyPublisher<[Product], Error> {
+    func loadProducts(pageNumber: Int, count: Int) -> AnyPublisher<[Product], Error> {
         return productsListRepository
-            .fetchData(pageNumber: pageNumber, count: elementCount, type: ProductsResponse.self)
+            .fetchData(pageNumber: pageNumber, count: count, type: ProductsResponse.self)
             .map(\.items)
             .eraseToAnyPublisher()
     }
--- OpenMarket_MVVM/OpenMarket_MVVM/Sources/ViewModel/ProductListView/ProductListViewModel.swift
+++ OpenMarket_MVVM/OpenMarket_MVVM/Sources/ViewModel/ProductListView/ProductListViewModel.swift
@@ -8,7 +8,9 @@ import Combine
 import Foundation
 
 class ProductListViewModel {
-    private var pageNumber = 1
+    private var pageNumber: Int = 1
+    private var elementCount: Int = 30
+    
     @Published private(set) var products: [Product] = []
     
     private let apiService: ProductListServicing
@@ -20,13 +22,17 @@ class ProductListViewModel {
     }
     
     func fetchProducts() {
-        apiService.loadProducts()
+        apiService.loadProducts(pageNumber: pageNumber, count: elementCount)
             .sink { completion in
-                if case let .failure(error) = completion {
-                    print(error)
+                switch completion {
+                case .finished:
+                    self.pageNumber += 1
+                case .failure(let error):
+                    debugPrint(error)
                 }
-            } receiveValue: { self.products = $0 }
+            } receiveValue: { products in
+                self.products = products
+            }
             .store(in: &subscribers)
-
     }
 }
