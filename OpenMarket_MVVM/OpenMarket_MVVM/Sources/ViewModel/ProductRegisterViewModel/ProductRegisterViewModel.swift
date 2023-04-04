70a05d4cb5ce6fcf5d4bd0894730308096b34d3b
미니 <cow970814@naver.com>
2023-04-04 17:42:22 +0900

    feat : ViewModel 내 Service 생성 및 호출 메서드 생성

--- OpenMarket_MVVM/OpenMarket_MVVM/Sources/ViewModel/ProductRegisterViewModel/ProductRegisterViewModel.swift
+++ OpenMarket_MVVM/OpenMarket_MVVM/Sources/ViewModel/ProductRegisterViewModel/ProductRegisterViewModel.swift
@@ -13,6 +13,22 @@ final class ProductRegisterViewModel {
         var data: Data? = nil
     }
     
+    struct RegisterProduct: Codable {
+        let name: String
+        let description: String
+        let price: String
+        let currency: String
+        let discountedPrice: String
+        let stock: String
+        var secret: String = "mgf4rzxzpe4gkpf5"
+    }
+    
+    private let productListService: ProductListServicing
+    
+    init(productListService: ProductListServicing = ProductListService(productsListRepository: ProductListRepository())) {
+        self.productListService = productListService
+    }
+    
     private var selectedIndex: Int = 0
     @Published private(set) var imageDatas = CircularQueue<Data>(count: 5)
     @Published private(set) var imageItemDatas: [ImageItem] = [ImageItem()]
@@ -36,5 +52,34 @@ final class ProductRegisterViewModel {
     
     func updateSelectedIndex(with index: Int) {
         self.selectedIndex = index
+    }
+    
+    func generateParamsData() -> Data? {
+        let product = RegisterProduct(
+            name: name,
+            description: description ?? "",
+            price: price?.description ?? "1",
+            currency: currency.rawValue,
+            discountedPrice: bargainPrice?.description ?? "0",
+            stock: stock?.description ?? "0"
+        )
+        
+        let encoder = JSONEncoder()
+        
+        return try? encoder.encode(product)
+    }
+    
+    func registerProduct() {
+        guard let params = generateParamsData() else { return }
+        guard let identifier = "d94a4ffb-6941-11ed-a917-a7e99e3bb892".data(using: .utf8) else { return }
+        let images = imageItemDatas.compactMap(\.data)
+        
+        productListService.saveProduct(params: params, images: images, identifier: identifier)
+            .sink {
+                if $0 {
+                    print("Success save")
+                }
+            }
+            .store(in: &cancellables)
     }
 }
