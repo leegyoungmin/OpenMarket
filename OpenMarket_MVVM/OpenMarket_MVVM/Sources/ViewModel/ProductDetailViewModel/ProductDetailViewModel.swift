5e9fe136a2db8c16764d47ead624f3d504e5d16d
미니 <cow970814@naver.com>
2023-04-06 16:36:17 +0900

    feat : product Detail viewmodel 생성

--- OpenMarket_MVVM/OpenMarket_MVVM/Sources/ViewModel/ProductDetailViewModel/ProductDetailViewModel.swift
+++ OpenMarket_MVVM/OpenMarket_MVVM/Sources/ViewModel/ProductDetailViewModel/ProductDetailViewModel.swift
@@ -3,6 +3,17 @@
 //  OpenMarket_MVVM
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import Combine
+
+final class ProductDetailViewModel {
+    let id: Int
+    
+    init(id: Int) {
+        self.id = id
+    }
+    
+    func fetchDetailProduct() {
+        
+    }
+}
