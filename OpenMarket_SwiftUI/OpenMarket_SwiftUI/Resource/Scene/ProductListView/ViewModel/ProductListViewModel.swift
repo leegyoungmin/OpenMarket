e88f4fe1ef6b61c948aa5ba9a6f769fa0fb86c82
미니 <cow970814@naver.com>
2023-04-14 16:38:32 +0900

    feat : List View Model 로딩 상태 변수 생성 및 Pagination 메서드 생성

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/ProductListView/ViewModel/ProductListViewModel.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/ProductListView/ViewModel/ProductListViewModel.swift
@@ -8,22 +8,74 @@ import Foundation
 import Combine
 
 final class ProductListViewModel: ObservableObject {
-  private let marketWebRepository: MarketProductRepository
-  private var page: Int = 1
-  private var itemCount: Int = 10
-  private var cancellables = Set<AnyCancellable>()
-  
-  @Published var products: [Product] = []
-  
-  init(marketWebRepository: MarketProductRepository = MarketProductConcreteRepository()) {
-    self.marketWebRepository = marketWebRepository
-    fetchProducts()
-  }
-  
-  func fetchProducts() {
-    marketWebRepository.loadProducts(with: page, itemCount: 10)
-      .receive(on: DispatchQueue.main)
-      .assign(to: \.products, on: self)
-      .store(in: &cancellables)
-  }
+    enum FetchState {
+        case success
+        case loading
+        case endPage
+    }
+    
+    // Properties
+    private let marketWebRepository: MarketProductRepository
+    private var page: Int = 1
+    private var itemCount: Int = 10
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var state: FetchState = .success
+    @Published var products: [Product] = []
+    
+    // Initializer
+    init(marketWebRepository: MarketProductRepository = MarketProductConcreteRepository()) {
+        self.marketWebRepository = marketWebRepository
+        fetchProducts()
+    }
+    
+    func fetchProducts() {
+        toggleState()
+        
+        marketWebRepository.loadProducts(with: page, itemCount: itemCount)
+            .receive(on: DispatchQueue.main)
+            .assertNoFailure()
+            .sink {
+                self.toggleHasNextPage(with: $0.hasNext)
+                self.products.append(contentsOf: $0.items)
+                self.toggleState()
+            }
+            .store(in: &cancellables)
+    }
+    
+    func isLastItem(with item: Product) {
+        guard let lastItem = products.last else { return }
+        
+        let isLast = (lastItem.id == item.id)
+        
+        if isLast == false {
+            return
+        }
+        
+        toggleState()
+    }
+}
+
+private extension ProductListViewModel {
+    func toggleState() {
+        switch state {
+        case .loading:
+            state = .success
+        case .success:
+            state = .loading
+            
+        case .endPage:
+            break
+        }
+    }
+    
+    func toggleHasNextPage(with hasNext: Bool) {
+        if hasNext == true {
+            self.page += 1
+        }
+        
+        if hasNext == false {
+            self.state = .endPage
+        }
+    }
 }
