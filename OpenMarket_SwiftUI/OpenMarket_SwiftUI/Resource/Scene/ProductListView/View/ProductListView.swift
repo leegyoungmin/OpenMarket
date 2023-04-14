e9e1b178b3894934b576480f4217c26f720b5644
미니 <cow970814@naver.com>
2023-04-14 16:39:21 +0900

    feat : Pagination에 필요한 변수 선언 및 메서드 수행 (List View)

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/ProductListView/View/ProductListView.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/ProductListView/View/ProductListView.swift
@@ -8,54 +8,82 @@ import SwiftUI
 
 // MARK: Content View
 struct ProductListDisplayView: View {
-  @ObservedObject var viewModel: ProductListViewModel
-  @Binding var selectedSection: ListSection
-  
-  init(_ selectedSection: Binding<ListSection>) {
-    self._viewModel = ObservedObject(initialValue: ProductListViewModel())
-    self._selectedSection = selectedSection
-  }
-  
-  var body: some View {
-    if selectedSection == .list {
-      ProductListView(products: $viewModel.products)
-    } else {
-      ProductGridView(products: $viewModel.products)
+    @ObservedObject var viewModel: ProductListViewModel
+    @Binding var selectedSection: ListSection
+    
+    init(_ selectedSection: Binding<ListSection>) {
+        self._viewModel = ObservedObject(initialValue: ProductListViewModel())
+        self._selectedSection = selectedSection
     }
-  }
+    
+    var body: some View {
+        if selectedSection == .list {
+            ProductListView(viewModel: viewModel, products: $viewModel.products)
+        } else {
+            ProductGridView(products: $viewModel.products)
+        }
+    }
 }
 
 // MARK: Child View Components
 private extension ProductListDisplayView {
-  struct ProductListView: View {
-    @Binding var products: [Product]
-    
-    var body: some View {
-      List {
-        ForEach(products, id: \.self) { product in
-          ProductListCellView(product: product)
-            .listRowInsets(Constants.rowEdge)
+    struct ProductListView: View {
+        private var viewModel: ProductListViewModel
+        @Binding var products: [Product]
+        
+        init(viewModel: ProductListViewModel, products: Binding<[Product]>) {
+            self.viewModel = viewModel
+            self._products = .init(projectedValue: products)
         }
-        .listRowSeparator(.hidden)
-      }
-      .listStyle(.plain)
-    }
-  }
-  
-  struct ProductGridView: View {
-    @Binding var products: [Product]
-    
-    var body: some View {
-      ScrollView {
-        LazyVGrid(columns: Constants.columns) {
-          ForEach(products, id: \.self) { number in
-            ProductGridCellView()
-              .padding()
-          }
+        
+        var body: some View {
+            List {
+                ForEach(products, id: \.self) { product in
+                    ProductListCellView(product: product)
+                        .listRowInsets(Constants.rowEdge)
+                        .task { viewModel.isLastItem(with: product) }
+                }
+                .listRowSeparator(.hidden)
+                
+                if viewModel.state == .loading {
+                    ProductListDisplayView.loadingProgressView
+                        .task {
+                            viewModel.fetchProducts()
+                        }
+                }
+            }
+            .listStyle(.plain)
         }
-      }
     }
-  }
+    
+    struct ProductGridView: View {
+        // TODO: - GRID 뷰 로딩 및 item 설정하기
+        @Binding var products: [Product]
+        
+        var body: some View {
+            ScrollView {
+                LazyVGrid(columns: Constants.columns) {
+                    ForEach(products, id: \.self) { number in
+                        ProductGridCellView()
+                            .padding()
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension ProductListDisplayView {
+    static var loadingProgressView: some View {
+        HStack {
+            Spacer()
+            
+            ProgressView()
+                .progressViewStyle(.circular)
+            
+            Spacer()
+        }
+    }
 }
 
 // MARK: Name Space
