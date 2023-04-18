a751dce417ba54a70cff1c19e1c0f2e03a7ede9f
미니 <cow970814@naver.com>
2023-04-18 18:25:47 +0900

    feat : WebRepository에 Upload Network 메서드 추가

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
@@ -13,6 +13,11 @@ protocol WebRepository: AnyObject {
         endPoint: EndPointing,
         type: T.Type
     ) -> AnyPublisher<T, Error>
+    
+    func uploadRequestNetwork<T: Decodable>(
+        endPoint: EndPointing,
+        type: T.Type
+    ) -> AnyPublisher<T, Error>
 }
 
 enum APIError: Error {
@@ -36,6 +41,30 @@ extension WebRepository {
             headers: endPoint.headers
         )
         .validate()
+        .publishDecodable(type: type)
+        .tryCompactMap(\.value)
+        .eraseToAnyPublisher()
+    }
+    
+    func uploadRequestNetwork<T: Decodable>(
+        endPoint: EndPointing,
+        type: T.Type
+    ) -> AnyPublisher<T, Error> {
+        guard let url = try? endPoint.generateURL() else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        guard let body = endPoint.body else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        return AF.upload(
+            multipartFormData: body,
+            to: url,
+            method: endPoint.method,
+            headers: endPoint.headers
+        )
+        .validate(statusCode: 200...300)
         .publishDecodable(type: type)
         .tryCompactMap(\.value)
         .eraseToAnyPublisher()
