68a96992ee0ef522192d0410d24f54a78a38bd8e
미니 <cow970814@naver.com>
2023-04-18 15:56:06 +0900

    refactor : alamofire Network 메서드 수정

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
@@ -15,19 +15,29 @@ protocol WebRepository: AnyObject {
     ) -> AnyPublisher<T, Error>
 }
 
+enum APIError: Error {
+    case http(AFError)
+    case unknown
+}
+
 extension WebRepository {
     func requestNetwork<T: Decodable>(
         endPoint: EndPointing,
         type: T.Type
     ) -> AnyPublisher<T, Error> {
-        guard let request = endPoint.configureRequest() else {
+        guard let url = try? endPoint.generateURL() else {
             return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
         }
         
-        return AF.request(request)
-            .validate(statusCode: 200...300)
-            .publishDecodable(type: type)
-            .tryCompactMap(\.value)
-            .eraseToAnyPublisher()
+        return AF.request(
+            url,
+            method: endPoint.method,
+            parameters: endPoint.queries,
+            headers: endPoint.headers
+        )
+        .validate()
+        .publishDecodable(type: type)
+        .tryCompactMap(\.value)
+        .eraseToAnyPublisher()
     }
 }
