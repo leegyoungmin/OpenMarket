c770e898d2cc7f26c813bd4d98592a8b88d323c9
미니 <cow970814@naver.com>
2023-04-18 15:55:14 +0900

    feat : Alamofire 사용 및 Endpoint 수정

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/EndPointing.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/EndPointing.swift
@@ -4,39 +4,29 @@
 //
 //  Copyright (c) 2023 Minii All rights reserved.
 
+import Alamofire
 import Foundation
 
 protocol EndPointing {
   var baseURL: String { get }
   var path: String { get }
-  var method: String { get }
-  var headers: [String: String] { get }
-  var queries: [String: String] { get }
-  var body: Data? { get }
-}
-
-extension Dictionary where Key == String, Value == String {
-  var queryItems: [URLQueryItem] {
-    return self.map { URLQueryItem(name: $0.key, value: $0.value) }
-  }
+  var method: HTTPMethod { get }
+  var headers: HTTPHeaders { get }
+  var queries: Parameters { get }
+  var body: MultipartFormData? { get }
+    
+    func generateURL() throws -> URL
 }
 
 extension EndPointing {
-  func configureRequest() -> URLRequest? {
-    var components = URLComponents(string: baseURL)
-    
-    components?.path = path
-    components?.queryItems = queries.queryItems
-    
-    guard let url = components?.url else { return nil }
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = method
-    headers.forEach {
-      request.setValue($0.value, forHTTPHeaderField: $0.key)
+    func generateURL() throws -> URL {
+        var component = URLComponents(string: baseURL)
+        component?.path = path
+        
+        guard let url = try? component?.asURL() else {
+            throw APIError.unknown
+        }
+        
+        return url
     }
-    request.httpBody = body
-    
-    return request
-  }
 }
