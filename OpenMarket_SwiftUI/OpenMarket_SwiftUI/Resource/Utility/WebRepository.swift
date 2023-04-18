b6d207a9971b14175b034f653851f2f4df461a3e
미니 <cow970814@naver.com>
2023-04-18 14:57:39 +0900

    feat : Alamofire 추가 및 Alamofire 및 Combine 적용

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
@@ -4,28 +4,30 @@
 //
 //  Copyright (c) 2023 Minii All rights reserved.
 
+import Alamofire
 import Combine
 import Foundation
 
 protocol WebRepository: AnyObject {
-  func requestNetwork<T: Decodable>(
-    endPoint: EndPointing,
-    type: T.Type
-  ) -> AnyPublisher<T, Error>
+    func requestNetwork<T: Decodable>(
+        endPoint: EndPointing,
+        type: T.Type
+    ) -> AnyPublisher<T, Error>
 }
 
 extension WebRepository {
-  func requestNetwork<T: Decodable>(
-    endPoint: EndPointing,
-    type: T.Type
-  ) -> AnyPublisher<T, Error> {
-    guard let request = endPoint.configureRequest() else {
-      return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+    func requestNetwork<T: Decodable>(
+        endPoint: EndPointing,
+        type: T.Type
+    ) -> AnyPublisher<T, Error> {
+        guard let request = endPoint.configureRequest() else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        return AF.request(request)
+            .validate(statusCode: 200...300)
+            .publishDecodable(type: type)
+            .tryCompactMap(\.value)
+            .eraseToAnyPublisher()
     }
-    
-    return URLSession.shared.dataTaskPublisher(for: request)
-      .map(\.data)
-      .decode(type: T.self, decoder: JSONDecoder())
-      .eraseToAnyPublisher()
-  }
 }
