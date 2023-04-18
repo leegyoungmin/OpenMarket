8cf1f6d6150b5ce0ce82aa77a2659d6b340f8bc9
미니 <cow970814@naver.com>
2023-04-18 18:27:23 +0900

    feat : AddNewProductViewModel Repository 의존성 주입 및 uploadProduct 메서드 추가

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/AddNewProductView/ViewModel/AddNewProductViewModel.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/AddNewProductView/ViewModel/AddNewProductViewModel.swift
@@ -16,6 +16,33 @@ final class AddNewProductViewModel: ObservableObject {
     @Published var description: String = ""
     @Published var isPresentPhotoPicker: Bool = false
     @Published var images: [Data] = []
+    private var cancellables = Set<AnyCancellable>()
+    
+    private var marketRepository: MarketProductRepository
+    
+    init(marketRepository: MarketProductRepository = MarketProductConcreteRepository()) {
+        self.marketRepository = marketRepository
+    }
+    
+    func uploadProduct() {
+        let product = RequestProduct(
+            name: name,
+            description: description,
+            price: price,
+            currency: selectedCurrency.rawValue,
+            discountedPrice: discountedPrice,
+            stock: stock
+        )
+        guard let encodeData = product.encodingData() else { return }
+        marketRepository.uploadProduct(with: encodeData, images: images)
+            .sink { completion in
+                print(completion)
+            } receiveValue: { _ in
+                print("success")
+            }
+            .store(in: &cancellables)
+
+    }
     
     func updateImage(with data: Data) {
         if images.count == 5 { return }
