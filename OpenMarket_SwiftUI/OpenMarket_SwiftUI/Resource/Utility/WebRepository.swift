536c14a7782e1fa492458c98e1a4f801aa74df77
미니 <cow970814@naver.com>
2023-04-20 18:06:34 +0900

    style : 컨벤션 정리

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/WebRepository.swift
@@ -9,54 +9,54 @@ import Combine
 import Foundation
 
 protocol WebRepository: AnyObject {
-    func requestNetwork<T: Decodable>(
-        endPoint: EndPointing,
-        type: T.Type
-    ) -> AnyPublisher<T, Error>
+  func requestNetwork<T: Decodable>(
+    endPoint: EndPointing,
+    type: T.Type
+  ) -> AnyPublisher<T, Error>
 }
 
 enum APIError: Error {
-    case http(AFError)
-    case unknown
+  case http(AFError)
+  case unknown
 }
 
 extension WebRepository {
-    func requestNetwork<T: Decodable>(
-        endPoint: EndPointing,
-        type: T.Type
-    ) -> AnyPublisher<T, Error> {
-        guard let url = try? endPoint.generateURL() else {
-            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
-        }
-        
-        switch endPoint.method {
-        case .post:
-            let request = AF.upload(
-                multipartFormData: endPoint.body,
-                to: url,
-                method: endPoint.method,
-                headers: endPoint.headers
-            )
-            return execute(with: request, type: type)
-        default:
-            let request = AF.request(
-                url,
-                method: endPoint.method,
-                parameters: endPoint.queries,
-                headers: endPoint.headers
-            )
-            
-            return execute(with: request, type: type)
-        }
+  func requestNetwork<T: Decodable>(
+    endPoint: EndPointing,
+    type: T.Type
+  ) -> AnyPublisher<T, Error> {
+    guard let url = try? endPoint.generateURL() else {
+      return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
     }
+    
+    switch endPoint.method {
+    case .post:
+      let request = AF.upload(
+        multipartFormData: endPoint.body,
+        to: url,
+        method: endPoint.method,
+        headers: endPoint.headers
+      )
+      return execute(with: request, type: type)
+    default:
+      let request = AF.request(
+        url,
+        method: endPoint.method,
+        parameters: endPoint.queries,
+        headers: endPoint.headers
+      )
+      
+      return execute(with: request, type: type)
+    }
+  }
 }
 
 private extension WebRepository {
-    func execute<T: Decodable>(with request: DataRequest, type: T.Type) -> AnyPublisher<T, Error> {
-        request
-            .validate(statusCode: 200...300)
-            .publishDecodable(type: type)
-            .tryCompactMap(\.value)
-            .eraseToAnyPublisher()
-    }
+  func execute<T: Decodable>(with request: DataRequest, type: T.Type) -> AnyPublisher<T, Error> {
+    request
+      .validate(statusCode: 200...300)
+      .publishDecodable(type: type)
+      .tryCompactMap(\.value)
+      .eraseToAnyPublisher()
+  }
 }
