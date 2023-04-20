63552ed114ffd6d3ec512eb8e2086f382153894b
미니 <cow970814@naver.com>
2023-04-20 17:08:36 +0900

    feat : Detail Product View 기본 구현

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/DetailProductView/DetailProductView.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/DetailProductView/DetailProductView.swift
@@ -7,15 +7,58 @@
 import SwiftUI
 
 struct DetailProductView: View {
+  let product: Product
+  let list = ["a", "b", "c", "d", "e"]
   var body: some View {
-    VStack {
-      Text("Hello World!")
+    ScrollView(showsIndicators: false) {
+      VStack(alignment: .leading, spacing: 10) {
+        TabView {
+          ForEach(list, id: \.self) { _ in
+            Image(systemName: "person.circle")
+              .resizable()
+              .scaledToFit()
+              .frame(width: 300, height: 300, alignment: .center)
+          }
+        }
+        .frame(height: 300)
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+        
+        HStack(alignment: .bottom) {
+          Text(product.name)
+            .font(.system(size: 25, weight: .bold, design: .default))
+          
+          Spacer()
+          
+          Text("남은 수량 : \(product.stock)")
+            .foregroundColor(.secondary)
+        }
+        .padding(10)
+        
+        HStack {
+          Spacer()
+          
+          VStack(alignment: .leading) {
+            Text(product.priceDescription)
+              .foregroundColor(.red)
+              .strikethrough()
+            
+            Text(product.bargainPriceDescription)
+          }
+        }
+        .padding(10)
+        
+        Text(product.description)
+          .multilineTextAlignment(.leading)
+          .padding(10)
+      }
     }
   }
 }
 
 struct DetailProductView_Previews: PreviewProvider {
+  static let mockData = Product.mockData
   static var previews: some View {
-    DetailProductView()
+    DetailProductView(product: mockData)
   }
 }
