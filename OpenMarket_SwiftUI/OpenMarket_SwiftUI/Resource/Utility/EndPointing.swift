0473ce80d3afae8415d36a83bfac82e82d8faf60
미니 <cow970814@naver.com>
2023-04-22 12:22:26 +0900

    style : EndPointing 타입 컨벤션 정리

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/EndPointing.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/EndPointing.swift
@@ -8,25 +8,25 @@ import Alamofire
 import Foundation
 
 protocol EndPointing {
-    var baseURL: String { get }
-    var path: String { get }
-    var method: HTTPMethod { get }
-    var headers: HTTPHeaders { get }
-    var queries: Parameters { get }
-    var body: MultipartFormData { get }
-    
-    func generateURL() throws -> URL
+  var baseURL: String { get }
+  var path: String { get }
+  var method: HTTPMethod { get }
+  var headers: HTTPHeaders { get }
+  var queries: Parameters { get }
+  var body: Any? { get }
+  
+  func generateURL() throws -> URL
 }
 
 extension EndPointing {
-    func generateURL() throws -> URL {
-        var component = URLComponents(string: baseURL)
-        component?.path = path
-        
-        guard let url = try? component?.asURL() else {
-            throw APIError.unknown
-        }
-        
-        return url
+  func generateURL() throws -> URL {
+    var component = URLComponents(string: baseURL)
+    component?.path = path
+    
+    guard let url = try? component?.asURL() else {
+      throw APIError.unknown
     }
+    
+    return url
+  }
 }
