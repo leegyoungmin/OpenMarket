c34cbbe0f6e06231bd34867838ca023ee23c6bd9
미니 <cow970814@naver.com>
2023-04-27 17:50:58 +0900

    feat : RecommendList View 생성

---
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/HomeScene/RecommendListView/RecommendListViewModel/RecommendListViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  RecommendListViewModel.swift
+//  OpenMarket_SwiftUI
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import Combine
+
+final class RecommendListViewModel: ObservableObject {
+  private let marketRepository: MarketProductRepository
+  private var cancellables = Set<AnyCancellable>()
+  
+  @Published var recommends = [Product]()
+  
+  init(marketRepository: MarketProductRepository) {
+    self.marketRepository = marketRepository
+    
+    fetchRecommendProducts()
+  }
+  
+  func fetchRecommendProducts() {
+    if recommends.isEmpty == false {
+      recommends.removeAll()
+    }
+    
+    (1...10).forEach { index in
+      marketRepository.loadProducts(with: index, itemCount: 10)
+        .map(\.items)
+        .replaceError(with: [])
+        .sink { products in
+          let randomIndex = Int.random(in: 0..<10)
+          self.recommends.append(products[randomIndex])
+        }
+        .store(in: &cancellables)
+    }
+    
+  }
+}
