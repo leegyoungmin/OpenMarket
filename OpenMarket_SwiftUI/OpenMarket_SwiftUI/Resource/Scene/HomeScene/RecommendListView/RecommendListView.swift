58aec957c04270183fee8de3b7d5c333307a39b7
미니 <cow970814@naver.com>
2023-04-27 17:49:07 +0900

    feat : Recommend List View 생성

---
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/HomeScene/RecommendListView/RecommendListView.swift
@@ -0,0 +1,85 @@
+//
+//  RecommendListView.swift
+//  OpenMarket_SwiftUI
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import SwiftUI
+
+struct RecommendListView: View {
+  @StateObject private var viewModel: RecommendListViewModel
+  
+  init(viewModel: RecommendListViewModel) {
+    self._viewModel = StateObject(wrappedValue: viewModel)
+  }
+  
+  var body: some View {
+    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 2), alignment: .center) {
+      ForEach(viewModel.recommends) { product in
+        RecommendGridCell(product)
+      }
+    }
+    .padding(.horizontal, 16)
+    .padding(.bottom, 16)
+    .refreshable {
+      viewModel.fetchRecommendProducts()
+    }
+  }
+}
+
+private extension RecommendListView {
+  @ViewBuilder func RecommendGridCell(_ product: Product) -> some View {
+    ZStack {
+      AsyncImage(url: URL(string: product.thumbnail), content: { image in
+        image
+          .resizable()
+          .scaledToFill()
+      }, placeholder: {
+        ProgressView()
+          .progressViewStyle(.circular)
+      })
+      
+      favoriteButton
+    }
+    .frame(width: Constants.cellLength, height: Constants.cellLength)
+    .background(Color.white)
+    .cornerRadius(16)
+    .shadow(radius: 1)
+
+  }
+  
+  var favoriteButton: some View {
+    VStack {
+      HStack {
+        Spacer()
+        
+        Button {
+          print("Tapped")
+        } label: {
+          Image(systemName: "heart")
+        }
+        .padding(5)
+        .background {
+          Circle()
+            .fill(.white)
+        }
+      }
+      .padding()
+      
+      Spacer()
+    }
+  }
+}
+
+private extension RecommendListView {
+  enum Constants {
+    static let cellLength: CGFloat = 180
+  }
+}
+
+struct RecommendListView_Previews: PreviewProvider {
+  static let viewModel = RecommendListViewModel(marketRepository: MarketProductConcreteRepository())
+  static var previews: some View {
+    RecommendListView(viewModel: viewModel)
+  }
+}
