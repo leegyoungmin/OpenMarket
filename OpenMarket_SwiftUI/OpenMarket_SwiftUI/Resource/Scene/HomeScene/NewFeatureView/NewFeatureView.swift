adbe061ad74effbc17c90a6f03e6ee003708bc72
미니 <cow970814@naver.com>
2023-04-27 22:42:13 +0900

    feat : 홈 화면 최신 상품 영역 구현

---
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Scene/HomeScene/NewFeatureView/NewFeatureView.swift
@@ -0,0 +1,57 @@
+//
+//  NewFeatureView.swift
+//  OpenMarket_SwiftUI
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import SwiftUI
+
+struct NewFeatureView: View {
+  @StateObject var viewModel = NewFeatureViewModel(marketRepository: MarketProductConcreteRepository())
+  var body: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack {
+        ForEach(viewModel.recentProducts, id: \.id) { product in
+          newFeatureCell(with: product)
+        }
+      }
+      .padding(.horizontal, 16)
+    }
+  }
+}
+
+extension NewFeatureView {
+  @ViewBuilder func newFeatureCell(with product: Product) -> some View {
+    VStack(alignment: .leading) {
+      AsyncImage(url: URL(string: product.thumbnail)) { imagePhase in
+        imagePhase
+          .resizable()
+          .frame(width: 90, height: 90, alignment: .center)
+          .cornerRadius(16)
+      } placeholder: {
+        ProgressView()
+          .progressViewStyle(.circular)
+          .frame(width: 90, height: 90, alignment: .center)
+      }
+      
+      Text(product.vendorName)
+        .font(.system(.caption2))
+      
+      Text(product.name)
+        .lineLimit(2)
+        .font(.system(size: 13, weight: .thin))
+      
+      Text(product.bargainPriceDescription)
+        .minimumScaleFactor(0.5)
+        .font(.system(size: 16, weight: .medium))
+        .foregroundColor(.accentColor)
+    }
+    .frame(maxWidth: 90)
+  }
+}
+
+struct NewFeatureView_Previews: PreviewProvider {
+  static var previews: some View {
+    NewFeatureView()
+  }
+}
