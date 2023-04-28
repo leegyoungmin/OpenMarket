1a300ca8f80789de686f16de57a6d3f553cb2b89
미니 <cow970814@naver.com>
2023-04-28 10:35:18 +0900

    refactor : Search Text Field 오른쪽 취소 버튼 추가 및 포커싱에 따른 키보드 전환 메서드 추가

--- OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/ViewModifier/SearchTextField.swift
+++ OpenMarket_SwiftUI/OpenMarket_SwiftUI/Resource/Utility/ViewModifier/SearchTextField.swift
@@ -7,20 +7,51 @@
 import SwiftUI
 
 struct SearchTextField: View {
-  @Binding var searchText: String
   let placeHolder: String
+  @Binding var isPresentCancel: Bool
+  @Binding var searchText: String
+  
+  @FocusState private var focusedSearch: Bool
   
   var body: some View {
     HStack {
-      Image(systemName: "magnifyingglass")
-        .foregroundColor(.white)
+      HStack {
+        Image(systemName: "magnifyingglass")
+          .foregroundColor(.white)
+        
+        TextField(placeHolder, text: $searchText)
+          .focused($focusedSearch)
+      }
+      .padding(16)
+      .background(
+        Color(uiColor: .secondarySystemFill)
+          .cornerRadius(16)
+      )
+      .onChange(of: focusedSearch) { newValue in
+        withAnimation {
+          isPresentCancel = newValue
+        }
+      }
       
-      TextField(placeHolder, text: $searchText)
+      if isPresentCancel {
+        Button("취소") {
+          withAnimation {
+            keyBoardHide()
+            focusedSearch.toggle()
+          }
+        }
+      }
     }
-    .padding(16)
-    .background(
-      Color(uiColor: .secondarySystemFill)
-        .cornerRadius(16)
+  }
+}
+
+private extension View {
+  func keyBoardHide() {
+    UIApplication.shared.sendAction(
+      #selector(UIResponder.resignFirstResponder),
+      to: nil,
+      from: nil,
+      for: nil
     )
   }
 }
